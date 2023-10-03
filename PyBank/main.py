1f2c5c7642447b7e7f6565e9b3f1fa64e3416d28
Molleigh <144710935+molleighH@users.noreply.github.com>
2023-10-03 16:51:45 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,33 @@
-#REMINDER: to create a folder in GitHub use "/" at the end of the folder name to transform into a folder
+# REMINDER: to create a folder in GitHub use "/" at the end of the folder name to transform into a folder
 
-#First, import os module - allows to create file path across operating systems 
+# First, import os module - allows to create file path across operating systems 
 import os 
 
-#Import module for reading csv files & set correct path to csv files 
+# Import module for reading csv files & set correct path to csv files 
 import csv 
-csvpath = os.path.join("Users", "molleighhughes", "Desktop", "MSU DATA ANALYSIS", "Module 3 Challenge", "Starter_Code", "PyBank", "Resources")
+budget_data = os.path.join("python-challge", "PyBank", "budget_data.csv")
 
 #Translate csv opening in Python 
-with open(csvpath) as csvfile: 
+with open(budget_data, newline="") as csvfile: 
 
-  #csv reader specifies delimiter (parameter tells Python each comma within csv should be seen as moving into a new column for a row) and variable that holds contents
-  csvreader = csv.reader(csvfile, delimiter=",") 
+    #csv reader specifies delimiter (parameter tells Python each comma within csv should be seen as moving into a new column for a row).
+    csvreader = csv.reader(csvfile, delimiter=",") 
 
-  print(csvreader)
+    #Read the header row first (skip this step if there is now header) 
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}") 
 
-  #Read the header row first (skip this step if there is now header) 
-  csv_header = next(csvreader)
-  print(f"CSV Header: {csv_header}") 
+# Variables  
+Profit = []
+Months = []
+Monthly_Change = []
 
-  #Read each row of data after the header 
-  for row in csvreader:
-      print(row) 
+
+    #Read each row of data after the header
+    for row in csvreader:
+        Profit.append(int(rows [1])) 
+        Months.append(rows[0])
+
+    #Find Monthly change 
+    for x in range(1, len(Profit)):
+        Monthly_Change.append((int(Profit[x])- int(Profit[x-1]))) 
